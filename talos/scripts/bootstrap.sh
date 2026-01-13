40e18a29d999adbef7411ac30644f3aba6714d2f
Mateusz Ledwoń <mateuszledwon@duck.com>
2026-01-13 08:52:00 +0100

    chore: Cleanup (#106)

--- talos/scripts/bootstrap.sh
+++ talos/scripts/bootstrap.sh
@@ -91,6 +91,8 @@ else
         --set hubble.relay.enabled=true \
         --set hubble.ui.enabled=true \
         --set operator.replicas=1 \
+        --set l2announcements.enabled=true \
+        --set externalIPs.enabled=true \
         --set securityContext.capabilities.ciliumAgent="{CHOWN,KILL,NET_ADMIN,NET_RAW,IPC_LOCK,SYS_ADMIN,SYS_RESOURCE,DAC_OVERRIDE,FOWNER,SETGID,SETUID}" \
         --set securityContext.capabilities.cleanCiliumState="{NET_ADMIN,SYS_ADMIN,SYS_RESOURCE}" \
         --set cgroup.autoMount.enabled=false \
